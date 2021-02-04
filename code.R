2271c573c29d678a622ba5052a5cf081240b7c74
shahrukh-ak <55116845+shahrukh-ak@users.noreply.github.com>
2021-02-04 12:17:55 -0800

    Code

--- code.R
+++ code.R
@@ -1,3 +1,5 @@
+#Testing whether price is dependent on the colour
+
 D=subset(data,colour=="D")
 I=subset(data,colour=="I")
 t.test(D$price,I$price)
@@ -13,3 +15,21 @@ sample estimates:
 7099.875 5175.025
 
 # From above test, the p value is higher than 0.05 so we failed to reject the null hypothesis and conclude that we don't have enough evidence to tell that price is dependent on colour.
+
+# Testing whether price is dependent on the clarity using high and low clarity levels.
+
+IF=subset(data,clarity=="IF")
+VS2=subset(data,clarity=="VS2")
+t.test(IF$price,alt="greater",VS2$price)
+
+Welch Two Sample t-test
+data: IF$price and VS2$price
+t = -5.1172, df = 92.098, p-value = 1
+alternative hypothesis: true difference in means is greater than 0
+95 percent confidence interval:
+  -4190.556 Inf
+sample estimates:
+  mean of x mean of y
+2694.773 5858.170 
+
+# From above test, the p value is higher than 0.05 so we failed to reject the null hypothesis and conclude that we don't have enough evidence to tell that price is dependent on clarity
